35d0b9df7c3ba5aeb92ab8f9227019d2763d3702
Nishat Nujhat <101088109+Nuzhat-here@users.noreply.github.com>
2024-10-01 18:45:03 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Navbar from './component/Navbar/Navbar';
-import Home from './component/Home/Home';
+import Home from './component/Home/Home.jsx';
 import Menu from './component/Menu/Menu.jsx';
 import Banner from './component/Banner/Banner.jsx';
 import Feedback from './component/Feedback/Feedback.jsx';
